febc284e2eaf24edee4cf81ff172135c4ade87e9
Vijay Dahiphale <vijay.dahiphale@msm.digital>
2020-03-10 19:42:42 +0530

    added sample class for validators

--- src/shared/models/user.model.ts
+++ src/shared/models/user.model.ts
@@ -1,3 +1,20 @@
+// import { IsNotEmpty, IsString, IsBoolean, IsOptional, IsEmail } from "class-validator";
+
+// export class UserLoginData {
+
+//     @IsString()
+//     @IsEmail()
+//     emailId: string;
+
+//     @IsString()
+//     @IsNotEmpty()
+//     password: string;
+
+//     @IsBoolean()
+//     @IsOptional()
+//     isEmailIdVerified?: boolean;
+// }
+
 export interface UserLoginData {
     emailId: string;
     password: string;
